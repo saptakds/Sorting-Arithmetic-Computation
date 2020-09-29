fcda42beb331b66c5c6eda45c44671227f1f1f79
saptakds <saptakds@gmail.com>
2020-09-29 15:19:02 +0530

    Added Storing computes in Dictionary

--- arithCompSort.sh
+++ arithCompSort.sh
@@ -6,3 +6,8 @@ compute1=$(($a+$b*$c))
 compute2=$(($a*$b+$c))
 compute3=$(echo "scale=2;$c+$a/$b" | bc)
 compute4=$(($a%$b+$c))
+declare -A computes
+computes[1]=$compute1
+computes[2]=$compute2
+computes[3]=$compute3
+computes[4]=$compute4
